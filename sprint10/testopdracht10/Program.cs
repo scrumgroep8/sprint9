3dd553c2b4a29b4a01144ef19acab555ecf36088
#32391614 <mpattipeilohy1@gmail.com>
2019-01-30 15:48:37 +0100

    ok

---
+++ sprint10/testopdracht10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testopdracht10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sprint12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sprint12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
