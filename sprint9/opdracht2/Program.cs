9d51668575dfe275e34842be09bb506d9d3ddb5b
#32391614 <mpattipeilohy1@gmail.com>
2019-02-01 13:57:00 +0100

    okehier

--- sprint9/opdracht2/Program.cs
+++ sprint9/opdracht2/Program.cs
@@ -4,34 +4,40 @@ namespace opdracht2
 {
     class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Clear();
 
-            //Login Attempts counter
-            int loginAttempts = 0;
+            //poging counter
+            int pogingen = 0;
 
-            //Simple iteration upto three times
+            string  naam;
+
+            Console.Write("typ je naam hier in; ");
+            naam = (Console.ReadLine());
+
+            //for loop die er voor zorgt dat je maar 3 keer kan
             for (int i = 0; i < 3; i++)
             {
-            Console.WriteLine("Enter username");
-            string username = Console.ReadLine();
+
             Console.WriteLine("Enter password");
             string password = Console.ReadLine();
 
-            if (username != "username" || password != "password")
-                loginAttempts++;
+            if (password != "password")
+                pogingen++;
             else
                 break;
             }
 
-            //Display the result
-            if (loginAttempts > 2)
-                Console.WriteLine("Login failure");
+            //resultaten
+            if (pogingen > 2)
+                Console.WriteLine("3 keer fout wachtwoord");
             else
-                Console.WriteLine("Login successful");
+                Console.WriteLine("je bent ingelogd");
+
+                Console.WriteLine("je naam is " + naam.ToString());
 
             Console.ReadKey();
         }
--- sprint9/opdracht3/Program.cs
+++ sprint9/opdracht3/Program.cs
@@ -6,7 +6,40 @@ namespace opdracht3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //poging counter
+            int pogingen = 0;
+
+            string  naam;
+
+            Console.Write("typ je naam hier in; ");
+            naam = (Console.ReadLine());
+
+            //for loop die er voor zorgt dat je maar 3 keer kan
+            for (int i = 0; i < 3; i++)
+            {
+
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+
+            if (password != "password")
+                pogingen++;
+            else
+                break;
+            }
+
+            //resultaten
+            if (pogingen > 2)
+                Console.WriteLine("3 keer fout wachtwoord");
+            else
+                Console.WriteLine("je bent ingelogd");
+
+                Console.WriteLine("je naam is " + naam.ToString());
+
+            Console.ReadKey();
         }
     }
 }
