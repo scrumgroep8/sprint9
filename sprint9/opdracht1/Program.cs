582c63803469cb43f7b77e007865ad684361d615
#32391614 <mpattipeilohy1@gmail.com>
2019-02-01 13:32:25 +0100

    ok

--- sprint9/opdracht1/Program.cs
+++ sprint9/opdracht1/Program.cs
@@ -4,9 +4,19 @@ namespace opdracht1
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            string  naam;
+
+            Console.Write("typ je naam hier in; ");
+            naam = (Console.ReadLine());
+
+            Console.WriteLine("je naam is " + naam.ToString());
+            Console.ReadLine();
         }
     }
 }
--- sprint9/opdracht2/Program.cs
+++ sprint9/opdracht2/Program.cs
@@ -4,9 +4,36 @@ namespace opdracht2
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //Login Attempts counter
+            int loginAttempts = 0;
+
+            //Simple iteration upto three times
+            for (int i = 0; i < 3; i++)
+            {
+            Console.WriteLine("Enter username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
+
+            if (username != "username" || password != "password")
+                loginAttempts++;
+            else
+                break;
+            }
+
+            //Display the result
+            if (loginAttempts > 2)
+                Console.WriteLine("Login failure");
+            else
+                Console.WriteLine("Login successful");
+
+            Console.ReadKey();
         }
     }
 }
