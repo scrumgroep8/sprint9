6d134802deed4be084804c01eacab06a3ff271b7
#32391614 <mpattipeilohy1@gmail.com>
2019-02-06 14:49:16 +0100

    opdracht 4 netjes gemaakt

--- sprint9/opdracht4/Program.cs
+++ sprint9/opdracht4/Program.cs
@@ -6,7 +6,80 @@ namespace opdracht4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //poging counter start bij 1
+            int pogingen = 1;
+            
+            //boolean die aangeeft of je wel of niet ingelogd bent
+            bool ingelogd = false;
+
+            //string voor je naam
+            string  naam;
+            //
+
+            //
+
+            //
+
+            // voorstukje waar je je naam invoert
+
+            Console.Write("typ je naam hier in; ");
+            naam = (Console.ReadLine());
+
+                //
+                
+                //
+
+                //
+
+                // for loop die er voor zorgt dat je maar 3 keer kan
+
+                for (int i = 0; i < 3; i++)
+                {
+                
+                Console.WriteLine("Enter password");
+                string password = Console.ReadLine();
+
+                Console.WriteLine("je hebt " + pogingen.ToString() + " keer geprobeerd");
+
+                if (pogingen == 2)
+                    {
+                        Console.WriteLine("LET OP JE DIT IS JE LAATSTE KANS");
+                    }
+
+                if (password != "SHARPSOUND")
+                    {
+                    pogingen++;
+                    }
+                else
+                    {
+                    break;
+                    }
+                }
+
+                    //
+                    
+                    //
+
+                    //
+
+                    // resultaten
+
+                    if (pogingen > 3)
+                    {
+                        Console.WriteLine("jammer te vaak fout");
+                    }
+                    else
+                    {
+                        Console.WriteLine("je bent ingelogd");
+
+                        Console.WriteLine("je naam is " + naam.ToString());
+
+                        ingelogd = true;
+                    }
         }
     }
 }
